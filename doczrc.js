4450eaab8a22aef8306ebcc55cbc1a8bdd9209e7
Kris Shinn <raggamuffin.music@gmail.com>
2019-02-12 23:32:42 -0800

    Reformated for docz

---
+++ doczrc.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'EthDenver 2019',
+  description: 'Documentation for EthDenver 2019',
+  themeConfig: {
+    logo: {
+      src: '/public/infura_logo_red.png',
+      width: 150
+    },
+    colors: {
+      sidebarBg: "#D4D4D4"
+    }
+  },
+  public: './public'
+}
