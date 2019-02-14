1e25807012c7f736086821ea367f3de48928d363
Kris Shinn <raggamuffin.music@gmail.com>
2019-02-14 14:39:51 -0800

    Adjustments for deployment; fill out docs

--- doczrc.js
+++ doczrc.js
@@ -1,9 +1,12 @@
+import emoji from 'remark-emoji';
+
 export default {
+  base: "/docs/",
   title: 'EthDenver 2019',
   description: 'Documentation for EthDenver 2019',
   themeConfig: {
     logo: {
-      src: '/public/infura_logo_red.png',
+      src: '/docs/public/infura_logo_red.png',
       width: 100
     },
     colors: {
@@ -17,5 +20,6 @@ export default {
       }
     }
   },
+  mdPlugins: [emoji],
   public: './public'
 }
